7326406174f7dcc90ab6c992fd2b44b5dd878b69
natek-1 <nategabrielk@icloud.com>
2025-09-25 17:47:44 -0400

    updated training file location

--- train/player_train.py
+++ train/player_train.py
@@ -19,6 +19,9 @@ for type in {"train", "valid"}:
     shutil.move(f"{dataset_path}/{type}", f"datasets/{dataset_path}/{dataset_path}/{type}")
 
 
-'''
+''' # train for player
 yolo task=detect mode=train model=yolov5l6u.pt data=./Basketball-Players-25/data.yaml epochs=100 imgsz=640 plots=True batch=16
+'''
+''' train for ball detection
+yolo task=detect mode=train model=yolov5l6u.pt data=./Basketball-Players-25/data.yaml epochs=250 imgsz=640 plots=True batch=16
 '''
