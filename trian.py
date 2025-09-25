f60863ceeba338ee06243fb51fadbde469ca3fdc
natek-1 <nategabrielk@icloud.com>
2025-09-25 09:35:31 -0400

    udpated code to train model

--- trian.py
+++ trian.py
@@ -12,12 +12,13 @@ version = project.version(25)
 dataset = version.download("yolov5")
 
 dataset_path = dataset.location.split('/')[-1]
+print(dataset.location)
+
+# required for yolov 5
+for type in {"train", "valid"}:
+    shutil.move(f"{dataset_path}/{type}", f"datasets/{dataset_path}/{dataset_path}/{type}")
+
 
 '''
-for type in {"train", "valid", "test"}:
-    shutil.move(f"{dataset_path}/{type}", f"{dataset_path}/{dataset_path}/{type}")
-'''
-
-'''
-yolo task=detect mode=train model=yolov516u.pt dataset={dataset.location}/data.yaml epochs=100 imsz=640 plots=True batch=16
+yolo task=detect mode=train model=yolov5l6u.pt data=./Basketball-Players-25/data.yaml epochs=100 imgsz=640 plots=True batch=16
 '''
