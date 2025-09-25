6e43d2458ed80af60b3678bd82a397f1e37ef035
Nathan Gabriel <nategabrielk@icloud.com>
2025-09-25 08:53:44 -0400

    updated train file

--- trian.py
+++ trian.py
@@ -1,4 +1,6 @@
 import os 
+import shutil
+
 from roboflow import Roboflow
 from dotenv import load_dotenv
 
@@ -8,4 +10,14 @@ rf = Roboflow(api_key=os.environ['ROBOFLOW_API_KEY'])
 project = rf.workspace("roboflow-universe-projects").project("basketball-players-fy4c2")
 version = project.version(25)
 dataset = version.download("yolov5")
-                
+
+dataset_path = dataset.location.split('/')[-1]
+
+'''
+for type in {"train", "valid", "test"}:
+    shutil.move(f"{dataset_path}/{type}", f"{dataset_path}/{dataset_path}/{type}")
+'''
+
+'''
+yolo task=detect mode=train model=yolov516u.pt dataset={dataset.location}/data.yaml epochs=100 imsz=640 plots=True batch=16
+'''
