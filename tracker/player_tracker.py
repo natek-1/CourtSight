37a448e4e0b2bd84e3a65e65c1d4382186eaaac9
Nathan Gabriel <nategabrielk@icloud.com>
2025-09-26 06:36:19 -0400

    added file for player tracking

---
+++ tracker/player_tracker.py
@@ -0,0 +1,44 @@
+
+
+from ultralytics import YOLO
+import supervision as sv
+
+class PlayerTracker:
+    
+    def __init__(self, model_path, device):
+        self.model = YOLO(model_path).to(device)
+        self.tracker = sv.ByteTrack()
+        
+    
+    def detect_frames(self, frames, batch_size=32, conf=0.5):
+        detections = []
+        for i in range(0, len(frames), batch_size):
+            batch_frames = frames[i:i+batch_size]
+            predictions = self.model.predict(batch_frames, conf=conf)
+            detections += predictions
+        return detections
+
+    def get_object_track(self, frames, batch_size=32, conf=0.5):
+        detections = self.detect_frames(frames, batch_size=batch_size, conf=conf)
+        tracks = []
+        
+        for frame_num, detection in enumerate(detections):
+            cls_name = detection.names
+            cls_to_idx = {classification:idx for idx,classification in cls_name.items()}
+
+            detection_supervision = sv.Detections.from_ultralytics(detection)
+            
+            detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
+            
+            tracks.append({})
+            
+            for frame_detection in detection_with_tracks:
+                bbox = frame_detection[0].tolist()
+                class_id = frame_detection[3]
+                track_id = frame_detection[4]
+                
+                if class_id == cls_to_idx['Player']:
+                    tracks[frame_num][track_id] = {"bbox":bbox}
+        
+        return tracks
+        
